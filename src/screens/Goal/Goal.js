418215f634a363fb054e14041335fd17981bdeef
gabrielrangel95 <gabriel.avila.rangel@gmail.com>
2018-07-11 15:57:33 -0300

    feat: backgroud images implement

--- src/screens/Goal/Goal.js
+++ src/screens/Goal/Goal.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import {
   MainContainer,
+  BeansImage,
+  DumbbellImage,
+  MatImage,
   Logo,
 } from './GoalStyle';
 
 const background = require('../../assets/img/backgroundGrain.png');
 const logo = require('../../assets/img/icon8Logo.png');
+const beansBackground = require('../../assets/img/imgBeans.png');
+const dumbbellBackground = require('../../assets/img/imgDumbbell.png');
+const matBackground = require('../../assets/img/imgMat.png');
 
 class Goal extends Component {
   static navigationOptions = {
@@ -15,6 +21,9 @@ class Goal extends Component {
   render() {
     return (
       <MainContainer source={background}>
+        <BeansImage source={beansBackground} />
+        <MatImage source={matBackground} />
+        <DumbbellImage source={dumbbellBackground} />
         <Logo source={logo} />
       </MainContainer>
     );
--- src/screens/Goal/GoalStyle.js
+++ src/screens/Goal/GoalStyle.js
@@ -7,13 +7,38 @@ const MainContainer = styled.ImageBackground`
   justify-content: center;
 `;
 
+const BeansImage = styled.Image`
+  height: 80%;
+  width: 50%;
+  position: absolute;
+  left: 0;
+`;
+
+const DumbbellImage = styled.Image`
+  height: 36%;
+  width: 29%;
+  position: absolute;
+  right: 0;
+  top: 50%;
+`;
+
+const MatImage = styled.Image`
+  height: 10%;
+  width: 30%;
+  position: absolute;
+  right: 0;
+  top: 80%;
+`;
+
 const Logo = styled.Image`
   width: 22px;
   height: 44px;
 `;
 
-
 export {
   MainContainer,
+  BeansImage,
+  DumbbellImage,
+  MatImage,
   Logo,
 };
