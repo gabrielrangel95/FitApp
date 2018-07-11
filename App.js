af00c3deadb1bb7bf1e7c117c835e09072157f02
gabrielrangel95 <gabriel.avila.rangel@gmail.com>
2018-07-11 15:44:41 -0300

    feat: load fonts and landing implement

--- App.js
+++ App.js
@@ -1,6 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { Font } from 'expo';
+import store from './src/redux/store';
 import { Router } from './src/router';
+import { Landing } from './src/screens';
 
-const App = () => <Router />;
+class App extends Component {
+  state = {
+    loading: true,
+  }
+
+  componentDidMount() {
+    console.disableYellowBox = true;
+    this.loadFonts();
+  }
+
+  loadFonts = async () => {
+    await Font.loadAsync({
+      firaSansBold: require('./src/assets/fonts/FiraSans-Bold.ttf'),
+      firaSansMedium: require('./src/assets/fonts/FiraSans-Medium.ttf'),
+    });
+    this.setState({ loading: false });
+  }
+
+  render() {
+    const { loading } = this.state;
+    return (
+      <Provider store={store}>
+        {
+          loading ? <Landing />
+          : <Router />
+        }
+      </Provider>
+    );
+  }
+}
 
 export default App;
