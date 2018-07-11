9fbf28b7d6228beb5a1714b5a9bfa26c2b98d5bd
gabrielrangel95 <gabriel.avila.rangel@gmail.com>
2018-07-11 15:40:23 -0300

    feat: create actions

---
+++ src/redux/actions/User.js
@@ -0,0 +1,22 @@
+import { Types } from '@redux/types';
+
+export const UserActions = {
+  setUserGoal: goal => ({
+    type: Types.USET_SET_GOAL,
+    payload: {
+      goal,
+    },
+  }),
+  setUserHeight: height => ({
+    type: Types.USER_SET_HEIGHT,
+    payload: {
+      height,
+    },
+  }),
+  setUserAge: age => ({
+    type: Types.USER_SET_AGE,
+    payload: {
+      age,
+    },
+  }),
+};
