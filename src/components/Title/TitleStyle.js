d0e4082ea9a8e43e4d24535611860d77b867e6ad
gabrielrangel95 <gabriel.avila.rangel@gmail.com>
2018-07-14 10:44:49 -0300

    ref: custom title

--- src/components/Title/TitleStyle.js
+++ src/components/Title/TitleStyle.js
@@ -2,8 +2,8 @@ import styled from 'styled-components/native';
 
 const TextTitle = styled.Text`
   font-family: firaSansBold;
-  font-size: ${props => (props.big ? 32 : props.medium ? 24 : 20)};
-  margin-bottom: ${props => (props.big ? 60 : props.medium ? 30 : 8)};
+  font-size: ${props => (props.medium ? 24 : 20)};
+  margin-bottom: ${props => (props.medium ? 30 : 8)};
   color: black;
 `;
 
