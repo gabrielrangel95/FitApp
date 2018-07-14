a9dd52f56a7de4e69d7bc723300b7f8e324bad1a
gabrielrangel95 <gabriel.avila.rangel@gmail.com>
2018-07-14 10:00:55 -0300

    feat: goal cards implement

--- src/screens/Goal/Goal.js
+++ src/screens/Goal/Goal.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import { Feather } from '@expo/vector-icons';
 import {
   MainContainer,
   BeansImage,
   DumbbellImage,
   MatImage,
   Logo,
+  ContentContainer,
+  SubTitle,
+  Title,
+  CellContainer,
+  CellTitle,
+  CellSubTitle,
+  IconContainer,
 } from './GoalStyle';
 
 const background = require('../../assets/img/backgroundGrain.png');
@@ -18,13 +26,46 @@ class Goal extends Component {
     header: null,
   };
 
+  onGoalPress = () => {
+    this.props.navigation.navigate('Steps');
+  }
+
   render() {
+    const iconSize = 20;
     return (
       <MainContainer source={background}>
         <BeansImage source={beansBackground} />
         <MatImage source={matBackground} />
         <DumbbellImage source={dumbbellBackground} />
         <Logo source={logo} />
+        <ContentContainer>
+          <SubTitle>WELCOME TO 8FIT</SubTitle>
+          <Title>What's your goal?</Title>
+
+          <CellContainer onPress={this.onGoalPress}>
+            <CellTitle>Lose weight</CellTitle>
+            <CellSubTitle>Burn fat & get lean</CellSubTitle>
+            <IconContainer>
+              <Feather name="chevron-right" size={iconSize} color="grey" />
+            </IconContainer>
+          </CellContainer>
+
+          <CellContainer>
+            <CellTitle>Get fitter</CellTitle>
+            <CellSubTitle>Tone up & feel healthy</CellSubTitle>
+            <IconContainer>
+              <Feather name="chevron-right" size={iconSize} color="grey" />
+            </IconContainer>
+          </CellContainer>
+          
+          <CellContainer>
+            <CellTitle>Gain muscle</CellTitle>
+            <CellSubTitle>Build mass & strength</CellSubTitle>
+            <IconContainer>
+              <Feather name="chevron-right" size={iconSize} color="grey" />
+            </IconContainer>
+          </CellContainer>
+        </ContentContainer>
       </MainContainer>
     );
   }
--- src/screens/Goal/GoalStyle.js
+++ src/screens/Goal/GoalStyle.js
@@ -4,18 +4,20 @@ const MainContainer = styled.ImageBackground`
   flex: 1;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
+  padding-top: 48;
 `;
 
 const BeansImage = styled.Image`
   height: 80%;
   width: 50%;
   position: absolute;
+  top: 16%;
   left: 0;
 `;
 
 const DumbbellImage = styled.Image`
-  height: 36%;
+  height: 38%;
   width: 29%;
   position: absolute;
   right: 0;
@@ -33,7 +35,77 @@ const MatImage = styled.Image`
 const Logo = styled.Image`
   width: 22px;
   height: 44px;
+  margin-bottom: 10;
 `;
+
+const ContentContainer = styled.View`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding-right: 20;
+  padding-left: 20;
+`;
+
+const SubTitle = styled.Text`
+  font-family: firaSansMedium;
+  font-size: 12;
+  color: black;
+  margin-bottom: 20;
+`;
+
+const Title = styled.Text`
+  font-family: firaSansBold;
+  font-size: 24;
+  color: black;
+  margin-bottom: 30;
+`;
+
+const CellContainer = styled.TouchableOpacity`
+  display: flex;
+  width: 100%;
+  height: 100;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  background-color: white;
+  margin-bottom: 20px;
+  padding-left: 20px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  padding-right: 10px;
+  border-radius: 12;
+  shadow-color: 'rgba(0, 0, 0, 0.3)',
+  shadow-opacity: 0.75;
+  shadow-radius: 5px;
+  shadow-offset: 2px 10px;
+`;
+
+const CellTitle = styled.Text`
+  font-family: firaSansBold;
+  font-size: 20;
+  color: black;
+  margin-bottom: 8;
+`;
+
+const CellSubTitle = styled.Text`
+  font-family: fireSansRegular;
+  font-size: 16;
+  color: grey;
+`;
+
+const IconContainer = styled.View`
+  position: absolute;
+  right: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  height: 100%;
+  width: 8%;
+`;
+
 
 export {
   MainContainer,
@@ -41,4 +113,11 @@ export {
   DumbbellImage,
   MatImage,
   Logo,
+  ContentContainer,
+  SubTitle,
+  Title,
+  CellContainer,
+  CellTitle,
+  CellSubTitle,
+  IconContainer,
 };
