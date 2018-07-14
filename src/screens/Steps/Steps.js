70853fab29f56a3e26beeaf71617fba68e206bed
gabrielrangel95 <gabriel.avila.rangel@gmail.com>
2018-07-14 11:19:08 -0300

    feat: step ui complete

--- src/screens/Steps/Steps.js
+++ src/screens/Steps/Steps.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { Feather } from '@expo/vector-icons';
-import { Stepper } from '@components';
+import { Stepper, Title, Button } from '@components';
 import {
   ArrowLeftContainer,
   Container,
+  DataContainer,
+  DataInput,
 } from './StepsStyle';
 
 
@@ -19,11 +21,25 @@ class Steps extends Component {
     ),
   })
 
+  state = {
+    age: 22,
+  }
 
   render() {
     return (
       <Container>
-        <Stepper steps={3} currentStep={1} />
+        <Stepper steps={2} currentStep={1} />
+        <DataContainer>
+          <Title medium>How old are you?</Title>
+          <DataInput
+            placeholder="Ex: 33"
+            keyboardType="number-pad"
+            value={this.state.age}
+            maxLength={3}
+            onChangeText={age => this.setState({ age })}
+          />
+          <Button text="Continue" />
+        </DataContainer>
       </Container>
     );
   }
--- src/screens/Steps/StepsStyle.js
+++ src/screens/Steps/StepsStyle.js
@@ -16,7 +16,26 @@ const Container = styled.View`
   background-color: white;
 `;
 
+const DataContainer = styled.KeyboardAvoidingView`
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 24;
+  margin-bottom: 20;
+  max-height: 54%;
+`;
+
+const DataInput = styled.TextInput`
+  font-family: firaSansBold;
+  font-size: 32;
+  color: black;
+`;
+
+
 export {
   ArrowLeftContainer,
   Container,
+  DataContainer,
+  DataInput,
 };
