d737d6aee90c878f0638ec0a57f1c33a46fcd7ea
gabrielrangel95 <gabriel.avila.rangel@gmail.com>
2018-07-14 10:01:51 -0300

    feat: implement steps screen

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,9 @@
 import { createStackNavigator } from 'react-navigation';
-import { Goal } from '@screens';
+import { Goal, Steps } from '@screens';
 
 const Router = createStackNavigator({
   Goal: { screen: Goal },
+  Steps: { screen: Steps },
 });
 
 export { Router };
