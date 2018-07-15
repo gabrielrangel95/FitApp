d2b7527ccc71bcb7f98978c1510afdf38906f5b4
gabrielrangel95 <gabriel.avila.rangel@gmail.com>
2018-07-15 18:09:51 -0300

    feat: new store implementing sagas

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,5 +1,18 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducers from '@redux/reducers';
+import createSagaMiddleware from 'redux-saga';
+import sagas from '@redux/sagas';
 
-const store = createStore(reducers);
+const sagaMonitor = __DEV__ ? console.tron.createSagaMonitor() : null;
+const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
+
+const middleware = [
+  sagaMiddleware,
+];
+
+const createAppropriateStore = __DEV__ ? console.tron.createStore : createStore;
+const store = createAppropriateStore(reducers, applyMiddleware(...middleware));
+
+sagaMiddleware.run(sagas);
+
 export default store;
