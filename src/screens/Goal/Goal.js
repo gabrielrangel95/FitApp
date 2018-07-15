41419136036bfdf64af91f7280815b8b4c478e83
gabrielrangel95 <gabriel.avila.rangel@gmail.com>
2018-07-15 16:31:54 -0300

    feat: data container animation

--- src/screens/Goal/Goal.js
+++ src/screens/Goal/Goal.js
@@ -11,6 +11,7 @@ import {
   IconContainer,
 } from './GoalStyle';
 import { Title } from '@components';
+import { AnimationFrameScheduler } from '../../../node_modules/rxjs/scheduler/AnimationFrameScheduler';
 
 const background = require('../../assets/img/backgroundGrain.png');
 const logo = require('../../assets/img/icon8Logo.png');
@@ -26,6 +27,8 @@ class Goal extends Component {
   componentWillMount() {
     this.beansImageLeftPosition = new Animated.Value(-100);
     this.rightImagesPosition = new Animated.Value(-100);
+    this.dataContainerOpacity = new Animated.Value(0);
+    this.dataContainerYPos = new Animated.Value(100);
   }
 
   componentDidMount() {
@@ -38,8 +41,15 @@ class Goal extends Component {
         toValue: 0,
         duration: 500,
       }),
+      Animated.timing(this.dataContainerOpacity, {
+        toValue: 1,
+        duration: 1000,
+      }),
+      Animated.timing(this.dataContainerYPos, {
+        toValue: 0,
+        duration: 1000,
+      }),
     ]).start();
-
   }
 
   onGoalPress = () => {
@@ -69,40 +79,50 @@ class Goal extends Component {
       right: this.rightImagesPosition,
       top: '50%',
     };
+    const dataContainerAnimStyle = {
+      flex: 1,
+      width: '100%',
+      opacity: this.dataContainerOpacity,
+      transform: [{
+        translateY: this.dataContainerYPos,
+      }],
+    };
     return (
       <MainContainer source={background}>
         <Animated.Image style={beansImgAnimStyle} source={beansBackground} />
         <Animated.Image style={matImagAnimStyle} source={matBackground} />
         <Animated.Image style={dumbbellImagAnimStyle} source={dumbbellBackground} />
         <Logo source={logo} />
-        <ContentContainer>
-          <SubTitle>WELCOME TO 8FIT</SubTitle>
-          <Title medium>What's your goal?</Title>
+        <Animated.View style={dataContainerAnimStyle}>
+          <ContentContainer>
+            <SubTitle>WELCOME TO 8FIT</SubTitle>
+            <Title medium>What's your goal?</Title>
 
-          <CellContainer onPress={this.onGoalPress}>
-            <Title>Lose weight</Title>
-            <CellSubTitle>Burn fat & get lean</CellSubTitle>
-            <IconContainer>
-              <Feather name="chevron-right" size={iconSize} color="grey" />
-            </IconContainer>
-          </CellContainer>
+            <CellContainer onPress={this.onGoalPress}>
+              <Title>Lose weight</Title>
+              <CellSubTitle>Burn fat & get lean</CellSubTitle>
+              <IconContainer>
+                <Feather name="chevron-right" size={iconSize} color="grey" />
+              </IconContainer>
+            </CellContainer>
 
-          <CellContainer>
-            <Title>Get fitter</Title>
-            <CellSubTitle>Tone up & feel healthy</CellSubTitle>
-            <IconContainer>
-              <Feather name="chevron-right" size={iconSize} color="grey" />
-            </IconContainer>
-          </CellContainer>
+            <CellContainer>
+              <Title>Get fitter</Title>
+              <CellSubTitle>Tone up & feel healthy</CellSubTitle>
+              <IconContainer>
+                <Feather name="chevron-right" size={iconSize} color="grey" />
+              </IconContainer>
+            </CellContainer>
 
-          <CellContainer>
-            <Title>Gain muscle</Title>
-            <CellSubTitle>Build mass & strength</CellSubTitle>
-            <IconContainer>
-              <Feather name="chevron-right" size={iconSize} color="grey" />
-            </IconContainer>
-          </CellContainer>
-        </ContentContainer>
+            <CellContainer>
+              <Title>Gain muscle</Title>
+              <CellSubTitle>Build mass & strength</CellSubTitle>
+              <IconContainer>
+                <Feather name="chevron-right" size={iconSize} color="grey" />
+              </IconContainer>
+            </CellContainer>
+          </ContentContainer>
+        </Animated.View>
       </MainContainer>
     );
   }
