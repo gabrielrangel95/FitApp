f8c3261cd1dc39fd36be745925d53f56ad18073e
gabrielrangel95 <gabriel.avila.rangel@gmail.com>
2018-07-16 09:58:17 -0300

    feat: create colors on style folder

---
+++ src/styles/colors.js
@@ -0,0 +1,19 @@
+export default {
+  white: '#FFF',
+  lighter: '#F2F2F2',
+  light: '#DDD',
+  regular: '#999',
+  dark: '#666',
+  darker: '#111',
+  black: '#000',
+
+
+  primary: '#1ed677',
+  danger: '#ff0000',
+
+
+  transparent: 'transparent',
+  whiteTransparent: 'rgba(255, 255, 255, 0.2)',
+  darkTransparent: 'rgba(0, 0, 0, 0.6)',
+  regularTransparent: 'rgba(0, 0, 0, 0.3)',
+};
