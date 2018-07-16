bf95415804701adc2ff347dba69dede62e2deb97
gabrielrangel95 <gabriel.avila.rangel@gmail.com>
2018-07-16 09:58:03 -0300

    feat: ui improves on sucess screen

--- src/screens/Success/Success.js
+++ src/screens/Success/Success.js
@@ -1,32 +1,86 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 // redux
 import { connect } from 'react-redux';
+
+// ui
+import { Animated, Easing } from 'react-native';
 import { Title } from '@components';
-import { Container, DescriptionText, DataRowContainer } from './SuccessStyle';
+import {
+  Styles,
+  Container,
+  DescriptionText,
+  DataRowContainer,
+  DataContainer,
+} from './SuccessStyle';
+
+const background = require('../../assets/img/backgroundGrain.png');
+const logo = require('../../assets/img/icon8Logo.png');
 
 class Success extends Component {
   static navigationOptions = {
     header: null,
   };
 
+  static propTypes = {
+    navigation: PropTypes.shape({
+      navigate: PropTypes.func,
+    }).isRequired,
+    user: PropTypes.shape({
+      goal: PropTypes.string,
+      age: PropTypes.string,
+      height: PropTypes.string,
+    }).isRequired,
+  };
+
+  componentWillMount() {
+    this.logoImgYPos = new Animated.Value(300);
+  }
+
+  componentDidMount() {
+    Animated.parallel([
+      Animated.timing(this.logoImgYPos, {
+        toValue: 0,
+        duration: 750,
+      }),
+    ]).start();
+  }
+
+  navigateToGoal = () => {
+    this.props.navigation.navigate('Goal');
+  }
+
   render() {
     const { goal, age, height } = this.props.user;
-    console.log(this.props.user);
+    const logoImgAnimStyle = {
+      width: 22,
+      height: 44,
+      marginTop: 48,
+      marginBottom: 24,
+      transform: [
+        {
+          translateY: this.logoImgYPos,
+        },
+      ],
+    };
     return (
-      <Container>
-        <Title medium>All Ready!</Title>
-        <Title>We have done a personalized training plan for you!</Title>
-        <DataRowContainer>
-          <Title>Goal: </Title>
-          <DescriptionText>{goal.title}</DescriptionText>
-        </DataRowContainer>
-        <DataRowContainer>
-          <Title>Age: </Title>
-          <DescriptionText>{age} years</DescriptionText>
-        </DataRowContainer>
-        <DataRowContainer>
-          <Title>Height: </Title>
-          {
+      <Container source={background}>
+        <Animated.Image style={logoImgAnimStyle} source={logo} />
+        <Animated.View style={Styles.dataContainer}>
+          <Title medium>All Ready!</Title>
+          <Title>We have done a personalized training plan for you!</Title>
+          <DataRowContainer>
+            <Title>Goal: </Title>
+            <DescriptionText>{goal.title}</DescriptionText>
+          </DataRowContainer>
+          <DataRowContainer>
+            <Title>Age: </Title>
+            <DescriptionText>{age} years</DescriptionText>
+          </DataRowContainer>
+          <DataRowContainer>
+            <Title>Height: </Title>
+            {
               (height.feet && height.inches) ? (
                 <DescriptionText>
                   {height.feet} Ft {height.inches} In
@@ -37,7 +91,8 @@ class Success extends Component {
                 </DescriptionText>
               )
             }
-        </DataRowContainer>
+          </DataRowContainer>
+        </Animated.View>
       </Container>
     );
   }
--- src/screens/Success/SuccessStyle.js
+++ src/screens/Success/SuccessStyle.js
@@ -1,11 +1,36 @@
 import styled from 'styled-components/native';
+import { colors } from '@styles';
+import { Dimensions, StyleSheet } from 'react-native';
 
-const Container = styled.View`
+const deviceWidth = Dimensions.get('window').width;
+
+const Styles = StyleSheet.create({
+  dataContainer: {
+    display: 'flex',
+    width: deviceWidth * 0.9,
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    backgroundColor: colors.white,
+    marginBottom: 20,
+    padding: 20,
+    borderRadius: 8,
+    shadowColor: colors.regularTransparent,
+    shadowOpacity: 0.75,
+    shadowRadius: 2,
+    shadowOffset: {
+      width: 6,
+      height: 6,
+    },
+  },
+});
+
+const Container = styled.ImageBackground`
   flex: 1;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
-  background-color: white;
+  justify-content: flex-start;
+  background-color: ${colors.white};
 `;
 
 const DataRowContainer = styled.View`
@@ -18,7 +43,12 @@ const DataRowContainer = styled.View`
 const DescriptionText = styled.Text`
   font-family: fireSansRegular;
   font-size: 16;
-  color: grey;
+  color: ${colors.regular};
 `;
 
-export { Container, DescriptionText, DataRowContainer };
+export {
+  Styles,
+  Container,
+  DescriptionText,
+  DataRowContainer,
+};
